033a1084036ef3132d1337181fa26f1c09d0d872
Brianna Bullock <brianna.bullock16@gmail.com>
2020-06-08 20:39:18 -0500

    added date at the top

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     var m = moment();
-    var topP = $(".lead");
-
-
-    // var topDate = $topP.text(m.format("dddd, MMMM, D"));
-
-    console.log(m.format("dddd, MMMM, D"));
+    var topP = $("#currentDay");
+    var topDate = topP.text(m.format("dddd, MMMM Do"));
 
 
 
