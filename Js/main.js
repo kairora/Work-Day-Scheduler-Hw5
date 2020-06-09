12ae66e68365cb5ab0c371a2fa5b2b2320d0fae1
Brianna Bullock <brianna.bullock16@gmail.com>
2020-06-09 01:10:13 -0500

    added hour element text to array

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
     var m = moment();
     var topP = $("#currentDay");
-    var topDate = topP.text(m.format("dddd, MMMM Do"));
+    var hour = $(".hour");
+    var hourEl = $(".hour").text();
+    var currentTime = m.format("HA");
+    var timeArr = [];
 
+    // sets the date at the top of the scheduler
+    topDate = topP.text(m.format("dddd, MMMM Do"));
 
-
-
-
+    // takes each hour's text and pushes into an array
+    hour.each(function(index, obj)
+    {
+    timeArr.push($(this).text());
+    });
 
 
 
